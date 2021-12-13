77b2c1f702485a8821828c0f9b48f4fa3c6dc8fc
Matthew Harris <rtpharry@hotmail.com>
2021-12-13 00:25:24 +0000

    initial commit

---
+++ jet-engine-repeater-values/dynamic-tags/jet-engine-repeater-dynamic-tag.php
@@ -0,0 +1,107 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+/**
+ * Elementor Dynamic Tag - Server Variable
+ *
+ * Elementor dynamic tag that returns a server variable.
+ *
+ * @since 1.0.0
+ */
+Class Elementor_Dynamic_Tag_Jet_Engine_Repeater extends \Elementor\Core\DynamicTags\Tag {
+
+	/**
+	 * Get dynamic tag name.
+	 *
+	 * Retrieve the name of the tag.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 * @return string Dynamic tag name.
+	 */
+	public function get_name() {
+		return 'runthings-jetengine-repeater-value';
+	}
+
+	/**
+	 * Get dynamic tag title.
+	 *
+	 * Returns the title of the tag.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 * @return string Dynamic tag title.
+	 */
+	public function get_title() {
+		return esc_html__( 'JetEngine Repeater Field', 'jet-engine-repeater-values' );
+	}
+
+	/**
+	 * Get dynamic tag groups.
+	 *
+	 * Retrieve the list of groups the tag belongs to.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 * @return array Dynamic tag groups.
+	 */
+	public function get_group() {
+		return [ 'runthings' ];
+	}
+
+	/**
+	 * Get dynamic tag categories.
+	 *
+	 * Retrieve the list of categories the tag belongs to.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 * @return array Dynamic tag categories.
+	 */
+	public function get_categories() {
+		return [ \Elementor\Modules\DynamicTags\Module::TEXT_CATEGORY ];
+	}
+
+	/**
+	 * Register dynamic tag controls.
+	 *
+	 * Add input fields to allow the user to customize the tag settings.
+	 *
+	 * @since 1.0.0
+	 * @access protected
+	 * @return void
+	 */
+	protected function register_controls() {
+		$this->add_control(
+			'repeater_key',
+			[
+				'type' => \Elementor\Controls_Manager::TEXT,
+				'label' => esc_html__( 'Repeater Key', 'jet-engine-repeater-values' ),
+			]
+		);
+	}
+
+	/**
+	 * Render tag output on the frontend.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 * @return void
+	 */
+	public function render() {
+		$field = $this->get_settings( 'repeater_key' );
+
+		if ( ! $field ) {
+			return;
+		}
+
+        if ( $field ) {
+            $result = jet_engine()->listings->data->get_meta($field);
+        }
+
+		echo wp_kses_post( $result );
+	}
+
+}
---
+++ jet-engine-repeater-values/jet-engine-repeater-values.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Plugin Name: JetEngine - Repeater field values
+ * Description: Add dynamic tag that returns a repeater variable.
+ * Plugin URI:  https://runthings.dev/
+ * Version:     1.0.0
+ * Author:      rtpHarry
+ * Author URI:  https://runthings.dev/
+ * Text Domain: jet-engine-repeater-values
+ *
+ * Elementor tested up to: 3.5.0
+ * Elementor Pro tested up to: 3.5.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+/**
+ * Register Repeater Variables Dynamic Tag Group.
+ *
+ * Register new dynamic tag group for Request Variables.
+ *
+ * @since 1.0.0
+ * @return void
+ */
+function register_dynamic_tag_group( $dynamic_tags ) {
+
+	\Elementor\Plugin::instance()->dynamic_tags->register_group(
+		'runthings',
+		[
+			'title' => esc_html__( 'RunThings.dev', 'jet-engine-repeater-values' )
+		]
+	);
+
+}
+add_action( 'elementor/dynamic_tags/register_tags', 'register_dynamic_tag_group' );
+
+/**
+ * Register Server Variable Dynamic Tag.
+ *
+ * Include dynamic tag file and register tag class.
+ *
+ * @since 1.0.0
+ * @return void
+ */
+function register_dynamic_tag( $dynamic_tags ) {
+
+	require_once( __DIR__ . '/dynamic-tags/jet-engine-repeater-dynamic-tag.php' );
+
+	$dynamic_tags->register_tag( 'Elementor_Dynamic_Tag_Jet_Engine_Repeater' );
+
+}
+add_action( 'elementor/dynamic_tags/register_tags', 'register_dynamic_tag' );
